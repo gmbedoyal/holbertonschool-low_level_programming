393d6a49d9d5d44e7b84eb3edafb7358f2a16203
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-09 21:35:33 -0500

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 int main(void) /*Descripción*/
 {
-prtinf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n")
 
 puts(str1);
 return (0);
