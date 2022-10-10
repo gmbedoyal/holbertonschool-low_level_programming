f42c2e7df11ef217506a6110802f3563efa1bdeb
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-09 20:39:14 -0500

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 #include <string.h>
 
 int main () {
    char str1[15];
 
-   strcpy(str1, ""Programming is like building a multilingual puzzle");
+   strcpy(str1, "Programming is like building a multilingual puzzle");
 
    puts(str1);
    
