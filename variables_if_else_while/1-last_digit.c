c155ba89621ecf5091edcbe35a58f9ab7cd39284
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-10 17:31:43 -0500

    task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("the last digit of %d is %d and is 0\n", n, n % 10)
+		printf("the last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	else
 	{
