46b96ecde8cd2057d76554b88a7aa22fc87e4dbf
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-10 09:49:40 -0500

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 /**
  * main - Descripción de la función main
- * str1 - Descripción del string
+ * 
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-char str1[55];
-
-strcpy(str1, "\"Programming is like building a multilingual puzzle");
-
-puts(str1);
-/**
- * Return: must return 0
- */
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
