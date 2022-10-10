a9a133ad9ab509b947b8174e072b124a6bdf398e
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-10 16:18:20 -0500

    task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
@@ -16,7 +16,7 @@ int main(void)
 	{
 		printf("is negative");
 	}
-	if else (n == 0)
+	else if (n == 0)
 	{
 		printf("is zero");
 	}
