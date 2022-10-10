6fc14752a415329e0e99d5f59ee3d32daad780c4
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-10 11:33:29 -0500

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("Size of a char: 1 byte(s)\n"
 "Size of an int: 4 byte(s)\n"
 "Size of a long int: 4 byte(s)\n"
 "Size of a long long int: 8 byte(s)\n"
-"Size of a float: 4 byte(s)");
+"Size of a float: 4 byte(s)\n");
 return (0);
 }
