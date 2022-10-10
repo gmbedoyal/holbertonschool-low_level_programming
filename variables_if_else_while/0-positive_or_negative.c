be46c90c63838aa941c076c9af4b21a7699c3df1
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-10 15:08:38 -0500

    task 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - descripción
+ * return - 0 if 0, <0 if negative, >0 if positive
+ **/i
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n<0) 
+	return ("is negative")
+	if (n = 0)
+	return ("is zero")
+	if (n > 0)
+	return ("is positive");
+}
