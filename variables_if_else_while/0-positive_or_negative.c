d78af2c6f8c528c64d291df2607a5353aa8ee2d1
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-10 16:14:01 -0500

    task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - descripción
  * return - 0 if 0, <0 if negative, >0 if positive
  **/
+
 int main(void)
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n<0)
+	if (n < 0)
 	{
 		printf("is negative");
 	}
