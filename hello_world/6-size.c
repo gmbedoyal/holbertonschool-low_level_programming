9864fa62be9b52b81aec2365d232da37485fdb27
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-10 10:36:17 -0500

    task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ *
+ *Return: debe ser 0
+ */
+int main(void)
+{
+printf("Size of a char: 1 byte(s)\n
+Size of an int: 4 byte(s)\n
+Size of a long int: 4 byte(s)\n
+Size of a long long int: 8 byte(s)\n
+Size of a float: 4 byte(s)\n");
+return (0);
+}
