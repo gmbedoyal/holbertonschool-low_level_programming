f276844fdcfc6780e7447ecc614fe551e86d9424
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-11 08:30:18 -0500

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - descripción de main
+ *
+ * Return: Always 0 (success)
+ */
+
+
+int main(void);
+{
+	char alphabet = 'a';
+	{
+	while (alphabet < z)
+		printf (%d, alphabet++);
+	}
+	
+return (0);
+}
