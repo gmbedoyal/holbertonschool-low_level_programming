f2b82d78d09b4c3461cbd4b4cc895beade4e1c50
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-11 11:17:58 -0500

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - descripción de main
+ *
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	int i;
+	{
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	}
+return (0);
+}
