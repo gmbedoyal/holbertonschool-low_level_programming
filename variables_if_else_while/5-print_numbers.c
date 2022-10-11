860b2ea8585892c3e207875a88d3c16b15163af4
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-11 10:40:01 -0500

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - descripción de main
+ *
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	int i;
+	{
+	for(i=0;i<10;i++)
+		printf("%d",i);
+	}
+	printf("\n");
+return (0);
+}
