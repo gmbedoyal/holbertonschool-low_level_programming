0e7fb761855efa680721732fbd49c8ef09363b2b
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-11 12:01:34 -0500

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - descripción de main
+ *
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	int n = 0;
+	char l = 'a';
+	{
+	while (n <= 15)
+		printf("%d", n++);
+	while (l <= 'f')
+		printf("%c", l++);
+	}
+	printf("\n");
+return (0);
+}
