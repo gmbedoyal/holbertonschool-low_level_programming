8b19a46745605b695934eee034c8fc61daf1ad0a
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-12 18:09:28 -0500

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int y;
+	for (y = '0'; y <= '9' ; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+		_putchar(x);
+		}
+	_putchar('\n');
+}
