81dccaae8b4244794768554d810cd2dd27473c88
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-12 18:02:37 -0500

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,12 +7,12 @@
  *
  * Return: Always 0.
  */
- 
-void print_alphabet (void)
+
+void print_alphabet(void)
 {
 	char x;
 
-	for (x='a'; x <= 'z'; x++)
+	for (x ='a'; x <= 'z'; x++)
 	{
 		_putchar(x);
 	}
