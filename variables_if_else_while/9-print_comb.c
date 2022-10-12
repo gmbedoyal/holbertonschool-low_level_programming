abd6018019d7055645cfb9f5f80b727cd5e0fd50
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-12 15:29:52 -0500

    task 0

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,17 +9,17 @@
 
 int main(void)
 {
-int i;
-for (i = '0'; i <= '9';)
-{
-putchar(i);
-if (i != '9')
-{
-putchar(',');
-putchar(' ');
-}
-i++;
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
