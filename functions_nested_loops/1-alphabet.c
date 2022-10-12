7caba3fb45fdb76d60c18f30b54346e91de59fb4
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-12 17:56:15 -0500

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,16 +3,16 @@
 #include <unistd.h>
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
  
 void print_alphabet (void)
 {
-	char x = 'a';
+	char x = "a";
 
-	while (x <= 'z')
+	while (x <= "z")
 	{
 		_putchar(x);
 		x++;
