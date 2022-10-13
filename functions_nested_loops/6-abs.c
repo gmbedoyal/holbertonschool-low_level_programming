7555ff3f7bbe14e83805dbcb25ee33f9195d762c
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-12 20:41:31 -0500

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *_isabs - check the code
+ *
+ * @i: número a evaluar
+ * Description - evaluate if alphabetic
+ * Return: 0 always.
+ */
+
+int _abs(int)
+{
+	int i = abs (i);
+	-putchar (i);
+
+	return (0);
+}
