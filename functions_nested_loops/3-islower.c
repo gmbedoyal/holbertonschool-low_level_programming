f5b7ec60e963936faff9b8fcb7dd394fbd083d50
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-12 20:11:33 -0500

    task 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,21 +3,21 @@
 #include <ctype.h>
 
 /**
- * int_islower - check the code
- *
+ * islower - check the code
+ * c - letra a evaluar
  * Return: 0 if uppercase, 1 if lowercase.
  */
 
 int _islower(int c)
 {
-	int d = islower (c);
+	int d = islower(c);
 
 	if (d == 0) /*si la letra es minúscula*/
 	{
 
 		return (0);
 	}
-	else 
+	else
 		return (1);
 }
 
