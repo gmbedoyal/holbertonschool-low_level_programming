d51c835a98c2cdf31a29f3f45482b8316ad8dc62
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-13 16:19:37 -0500

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_last_digit - check the code
+ *
+ * @i: número a evaluar
+ * Description - print last digit of a number
+ * Return: last digit.
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar(58);
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
