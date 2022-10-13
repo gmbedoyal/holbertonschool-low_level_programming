40f5ac688bafc4fcf74b12223b718186649627af
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-13 17:01:12 -0500

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -21,7 +21,7 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 		{
 			_putchar((i / 10) + '0');
-			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(58);
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
