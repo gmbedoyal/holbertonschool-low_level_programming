37d2e7bb1143f7f8eaa704782e1605fff7ffdf15
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-13 09:43:36 -0500

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *print_last_digit - check the code
+ *
+ * @i: número a evaluar
+ * Description - print last digit of a number
+ * Return: last digit.
+ */
+
+int print_last_digit(int i)
+{
+	int j = (i % 10);
+
+	return (j);
+	
+}
