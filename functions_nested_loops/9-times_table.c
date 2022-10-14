1eeb4ee990c5ec4db8ca9e947cbc6911bb791218
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-13 19:35:07 -0500

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,15 +15,27 @@ void times_table(void)
 	int i;
 	int j;
 	int k;
-	char l;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			k = (i * j);
-			l = (k);
-			_putchar(l);
+		k = (((i*j) / 10));
+			if (k >= 1)
+			{
+				_putchar(((i*j) / 10) + '0');
+				_putchar(((i*j) % 10) + '0');
+			} 
+			else
+			{
+				_putchar(((i*j) % 10) + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}	
 		}
 	_putchar('\n');
 	}
