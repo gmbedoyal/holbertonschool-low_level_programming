35acdf138097e2dce2c2bcb9e7f53c416639b28c
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-14 00:00:26 -0500

    task 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,15 +3,16 @@
 
 /**
  * add - check the code
- *
+ * i - valor a
+ * @j - valor b
  * Description - print last digit of a number
  * Return: last digit.
  */
 
 int add(int i, int j)
 {
-int k = (i+j);
+int k = (i + j);
 
-return(k);
+return (k);
 }
 
