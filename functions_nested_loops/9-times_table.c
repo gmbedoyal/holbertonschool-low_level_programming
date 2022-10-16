9ff9cbe98a52f581939a1b1e953373de6f8f4e39
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-16 07:49:01 -0500

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -21,8 +21,8 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 	{
-	m = (((i * j) / 10));
-	n = (((i * j) % 10));
+		m = (((i * j) / 10));
+		n = (((i * j) % 10));
 
 	if (i == 0)
 	{
@@ -40,58 +40,58 @@ void times_table(void)
 				_putchar(n + '0');
 				_putchar(',');
 			}
-                       if (j == 9)    
-                        {
-			    _putchar(' ');
-                            _putchar(' ');
-                            _putchar(n + '0');
-                        }
-                    }
-                }   
-	    else
-            {
-                if (j == 0)
-                    {
-                        _putchar(n + '0');
-                        _putchar (',');
-                    }
+			if (j == 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(n + '0');
+			}
+		}
+	}
+	else
+	{
+		if (j == 0)
+		{
+			_putchar(n + '0');
+			_putchar (',');
+		}
 		else
-            	{
-                	if ((m % 10) == 0)
-                	{
+		{
+			if ((m % 10) == 0)
+			{
 				if (j != 9) 
 				{
-                  		_putchar(' ');
-                  		_putchar(' ');
-                  		_putchar(n + '0');
-				_putchar(',');
-                		}
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(n + '0');
+					_putchar(',');
+				}
 				else
 				{
 				_putchar(' ');
-                		_putchar(' ');
-                  		_putchar(n + '0');
+				_putchar(' ');
+				_putchar(n + '0');
 				}
 			}
-                    else
-                    {
-			    if (j != 9)
-			    {    
-                      		_putchar (' ');
-                      		_putchar(m + '0');
-                      		_putchar(n + '0');
-                      		_putchar(',');
-			    }
-			    else
-			    {    
-                      		_putchar (' ');
-                      		_putchar(m + '0');
-                      		_putchar(n + '0');
-			    }
-                    }
+			else
+			{
+				if (j != 9)
+				{
+					_putchar (' ');
+					_putchar(m + '0');
+					_putchar(n + '0');
+					_putchar(',');
+				}
+				else
+				{
+					_putchar (' ');
+					_putchar(m + '0');
+					_putchar(n + '0');
+				}
+			}
 		}
-	    }	
-        }
+	}
+	}
 	_putchar('\n');
-    }
+	}
 }
