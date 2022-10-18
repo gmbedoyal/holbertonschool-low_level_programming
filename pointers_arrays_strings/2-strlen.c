05bf9a62a718efee4952e73fb3833cfc1859bc38
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-18 15:27:36 -0500

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strelen - check the code
+ *@s: pointer
+ *@b: pointer
+ *Return: check main.c
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(s);
+
+return(a);
+}
