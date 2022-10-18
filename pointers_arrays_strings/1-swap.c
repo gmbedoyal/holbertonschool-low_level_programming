835a125a694e38974418b569512c575e088eb409
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-18 15:07:48 -0500

    task 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- *reset_to_98 - check the code
+ *swap_int - check the code
  *@n: pointer
  *Return: check main.c
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+
+	int c = *a;
+
+	*a = *b;
+	*b = c;
 }
