18ffc07313ae9f2bc8af816eecd06becf5341449
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-18 17:35:21 -0500

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *print_rev - check the code
  *@str: pointer
  *Return: length of strenght
  */
 
-void _puts(char *str)
+void print_rev(char *s)
 {
 	int i = 0;
 	int j = 0;
 
-	while (str[i] != 0)
+	while (s[i] != 0)
 	{
-		_putchar(str[i]);
 		i++;
+	}
+
+	j = i - 1;
+	while (s[j] != 0)
+	
+	{
+		_putchar(s[j]);
+		j--;
 	}
 	_putchar('\n');
 }
