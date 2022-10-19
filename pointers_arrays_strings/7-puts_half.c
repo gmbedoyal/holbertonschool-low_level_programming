8ab717686781830ddf601d03dfbddf9d327c80b2
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-18 21:07:25 -0500

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *puts_half - check the code
+ *@str: pointer
+ *Return: length of strenght
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{		i++;}
+	while (*str)
+	{_putchar(*str+(i/2));
+		str++;}
+_putchar('\n');
+}
