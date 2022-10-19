75d281076c704fad64f5c6db804bedb59fc67136
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-19 14:56:29 -0500

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - check the code
+ *@a: pointer
+ *@n: integer
+ *Return: length of strenght
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{	
+		if (i == n - 1)
+			printf("%d", a[i]);
+		else	
+			printf("%d, ", a[i]);
+	}
+}
