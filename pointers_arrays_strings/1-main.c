671d57fb5677bbebfdf814441568c7ce6d2583d4
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-20 17:42:50 -0500

    task 1

--- pointers_arrays_strings/1-main.c
+++ pointers_arrays_strings/1-main.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
+    char s1[98] = "";
+    char s2[] = "";
     char *ptr;
 
     printf("%s\n", s1);
     printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
+    ptr = _strncat(s1, s2, 10);
     printf("%s\n", s1);
     printf("%s", s2);
     printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
+    ptr = _strncat(s1, s2, 10);
     printf("%s", s1);
     printf("%s", s2);
     printf("%s", ptr);
