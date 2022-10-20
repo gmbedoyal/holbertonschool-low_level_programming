b6663f2e2aec7d76e1594f12abf8d17f2d4f21e4
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-20 14:00:34 -0500

    task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -6,14 +6,10 @@ char *_strcat(char *dest, char *src)
 {
 char *p1 = &*dest;
 char *p2 = &*src; 
-//p1 = &dest;
-//p2 = &src;
 
 char s3 [99] = "0";
 char *p3 = &*s3;
 
-//*p3 = &s3;
-	
 *p3 = *p1 && *p2;
 
 return(p3);
