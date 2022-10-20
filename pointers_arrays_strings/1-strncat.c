e215ef14a0ca061bb74957d516aeb25d2e66a3b5
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-20 16:53:13 -0500

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - check code
+ * @dest: destination string
+ * @src: source string
+ * @n : integer
+ * Return: dest string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+
+while (dest[i] != '\0')
+{
+	i++;
+}
+
+for (j = 0; j <= n ; i++, j++)
+{
+	dest[i] = src[j];
+}
+dest[i] = '\0';
+
+return (dest);
+}
+
