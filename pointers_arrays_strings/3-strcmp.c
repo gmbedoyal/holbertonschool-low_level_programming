d3c60a1f22cd26a42d042ea66003172e8306cfa0
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-21 17:18:50 -0500

    task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,18 @@ int _strcmp(char *s1, char *s2)
 int i = 0;
 int k = 0;
 
-while (k == 0)
+if (s1[] != s2[])
 {
-	k = k + (s1[i] - s2[i]);
-	i++;
+	while (k == 0)
+	{
+		k = k + (s1[i] - s2[i]);
+		i++;
+	}
 }
-
-
+else
+{
+k = 0;
+}
 return (k);
 }
 
