d58db4c949b78f19c8caba47df53eb15493ab3eb
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-21 16:27:15 -0500

    task 1

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - check code
+ * @*a: pointer
+ * @n: integer
+ * Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+int * p = a;
+int i = 0;
+
+while (*a)
+	i++;
+
+*p = a[n] - a[i];
+}
+
