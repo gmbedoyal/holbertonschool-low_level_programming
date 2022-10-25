3c2b14f5f63cd7aa20448c407077e03364f710e2
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-25 15:22:27 -0500

    task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *_memset - check the code
+  *@s: pointer
+  *@b: character
+  *@n: unsigned int
+  *Return: returns a pointer to the memory area s
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+
+while (i <= n)
+{
+s[i] = b;
+i++;
+}
+return (s);
+}
