611a2b2702ca672c2e6c14245c030a38c17abb41
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-25 11:12:11 -0500

    task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,9 +8,8 @@
 char *leet(char *s)
 {
 int i = 0;
-char crypt[] = "43071";
-char AEOTL[] = "AEOTL";
-char aeotl[] = "aeotl";
+char crypt[] = "4307143071";
+char AEOTL[] = "AEOTLaeotl";
 int j;
 
 for (i = 0; s[i] != '\0'; i++)
@@ -18,10 +17,6 @@ for (i = 0; s[i] != '\0'; i++)
 	for (j = 0; AEOTL[j] != '\0' ; j++)
 	{
 		if (s[i] == AEOTL[j])
-			{
-			s[i] = crypt[j];
-			}
-		else if (s[i] == aeotl[j])
 			{
 			s[i] = crypt[j];
 			}
