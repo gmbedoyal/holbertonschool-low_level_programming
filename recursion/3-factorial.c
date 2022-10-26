13343db0361faa8751564145f3806677cc111094
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-26 11:09:14 -0500

    task 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *factorial - a function that returns the factorial of a given number
+  *@n: integer
+  *Return: if n is lower than 0, the function should return -1 to indicate an error, otherwise, factorial.
+  */
+
+int factorial(int n)
+{
+if (n == 0 || n == 1)
+{
+	return (1);
+}
+else if (n < 0)
+{
+	return -1;
+}
+else
+{
+	return (n*factorial (n-1));
+			}
+}
