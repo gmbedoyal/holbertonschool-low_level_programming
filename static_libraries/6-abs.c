8c63e8132e3c06da536f1c20d23de324538f0f2a
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-28 17:02:57 -0500

    task 0

---
+++ static_libraries/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *_abs - check the code
+ *
+ * @i: número a evaluar
+ * Description - evaluate if alphabetic
+ * Return: 0 always.
+ */
+
+int _abs(int i)
+{
+	int j = abs(i);
+
+	if (j >= 0)
+	{
+	return (j);
+	}
+	else
+	return (j);
+}
