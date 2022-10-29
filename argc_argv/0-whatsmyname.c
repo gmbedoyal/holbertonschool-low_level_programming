36c6f55d515bcae96ebfbf64684d824bbc294aa8
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-29 10:08:33 -0500

    task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+  *main - check the code
+  *@argc: integer
+  *@argv: vector
+  */
+
+int main(int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 (void) argc;
