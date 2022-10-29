ff5dda4d4818cb4979c361f9dbd00a3489ed67bd
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-29 11:41:05 -0500

    task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,29 +9,28 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
+	int suma = 0;
+
 	if (argc == 0)
 	{
 		printf("%d\n", 0);
 	return (1);
 	}
 
-	int i = 0;
-
-	for ( ; i < argc ; i++)
+	for (i = 0 ; i < argc ; i++)
 	{
 		if (atoi(argv[i])/atoi(argv[i]) != 1)
 			printf("Error\n");
 		return(1);
 	}
 
-	int suma = 0;
-	
 	for (i = 1 ; i < argc ; i++)
 	{
 		suma += atoi(argv[i]);
 
 	}
-	printf("%d\n", suma);
+	printf("%i\n", suma);
 
 return (0);
 }
