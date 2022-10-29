7fb76f5ce083f4b5c12a68bcea4b8a6aa310184f
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-28 19:42:08 -0500

    task 0

---
+++ recursion/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts - check the code
+ *@str: pointer
+ *Return: length of strenght
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
