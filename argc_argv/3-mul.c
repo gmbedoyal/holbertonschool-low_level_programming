5514f443ca1362b5995a4cb4d16100b666142408
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-29 11:09:46 -0500

    task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - check the code
+  *@argc: integer
+  *@argv: vector
+  *Return: zero
+  */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+	return (1);
+	}
+
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
