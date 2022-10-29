4d9e0ee7903eaee4fd39ab4332538f2d51f3b228
Miguel Bedoya <gmbl07@hotmail.com>
2022-10-29 10:23:09 -0500

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - check the code
+  *@argc: integer
+  *@argv: vector
+  *Return: zero
+  */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+/* cdfa*/
+(void) argv;
+return (0);
+}
