1ba92534c915562515212998586577796e8a9dc6
Miguel Bedoya <gmbl07@hotmail.com>
2022-11-01 16:14:57 -0500

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,42 +18,34 @@ unsigned int k = 0;
 unsigned int l = 0;
 
 if (s1 == NULL)
-{s1 = "";}
-
+{s1 = "";
+}
 if (s2 == NULL)
-{s2 = "";}
-
+{s2 = "";
+}
 while (s1[i] != '\0')
 {
 	i++;
 }
-
 while (s2[j] != '\0')
 {
 	j++;
 }
-
 dup = malloc(sizeof(*s1) * (i + j) + 1);
-
 if (dup == NULL)
 {	
 	return (NULL);
 }
-
 while (k < i)
 {
 dup[k] = s1[k];
 k++;
 }
-
 while (l < j)
 {
 dup[k + l] = s2[l];
 l++;
 }
-
 return (dup);
-
 free(dup);
-
 }
