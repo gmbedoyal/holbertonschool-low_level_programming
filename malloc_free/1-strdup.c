fb708c0c9eeb57046dc172798c6122cc7631b6ec
Miguel Bedoya <gmbl07@hotmail.com>
2022-11-01 14:35:08 -0500

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *_strdup - creates an array of chars
+  *@str: string
+  *Return: NULL if size 0 or pointer null
+  */
+
+char *_strdup(char *str)
+{
+char *dup;
+unsigned int i = 0;
+unsigned int j = 0;
+
+if (str == NULL)
+{
+	return (NULL);
+}
+
+while (str[i] != '\0')
+{
+	i++;
+}
+
+dup = malloc(sizeof(*str) * i);
+
+while (j < i)
+{
+dup[j] = str [j];
+j++;
+}
+return (dup);
+
+free(dup);
+
+}
