2c0abf075a291f0e5e9ef28c84dd66a4b2b5a32d
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-08 11:31:04 -0500

    task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - perro
+ * @name: nombre
+ * @age: edad
+ * @owner: due√o
+ *
+ * Descripci√n: datos de un perro
+ */
+
 struct dog
 {
 char *name;
