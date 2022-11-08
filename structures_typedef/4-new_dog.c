d643c81684cc6093fc5dc3a24fa678ee1ad0b69d
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-08 14:49:48 -0500

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  *dog_t - struct dog
- *@new_dog: pointer
+ *new_dog - pointer
  *@name: name
  *@age: age
  *@owner: owner
@@ -16,7 +16,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 puppy = malloc(sizeof(dog_t));
 
 if (puppy == NULL)
+{
 	return (NULL);
+	free(puppy);
+}
 
 puppy->name = name;
 puppy->age = age;
