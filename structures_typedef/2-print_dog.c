bdbe80a9363a437828035e7bd1e6e2a62a337180
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-08 13:31:09 -0500

    task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,16 +18,16 @@ if (d->name == NULL)
 }
 else
 {
-	printf("Name: %s\n", d-> name);
+	printf("Name: %s\n", d->name);
 }
-printf("Age: %f\n", d-> age);
+printf("Age: %f\n", d->age);
 
 if (d->owner == NULL)
-{	
+{
 	printf("Owner: (nil)\n");
 }
-else 
+else
 {
-	printf("Owner: %s\n", d-> owner);
+	printf("Owner: %s\n", d->owner);
 }
 }
