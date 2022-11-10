8c06c4d4c77315dd17010cc1014c92ca202010c2
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-10 15:55:31 -0500

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,53 +1,78 @@
-#include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * new_dog - initializa dog struct
- * @name: char *
- * @age: float
- * @owner: char *
- * Return: dog strct
+  *_strdup - creates an array of chars
+  *@str: string
+  *Return: NULL if size 0 or pointer null
+  */
+
+char *_strdup(char *str)
+{
+char *dup;
+unsigned int i = 0;
+unsigned int j = 0;
+
+if (str == NULL)
+{
+	return (NULL);
+}
+
+while (str[i] != '\0')
+{
+	i++;
+}
+
+dup = malloc(sizeof(*str) * i + 1);
+
+if (dup == NULL)
+{
+	free(dup);
+	return (NULL);
+}
+
+while (j < i)
+{
+dup[j] = str[j];
+j++;
+}
+
+return (dup);
+}
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *new_dog - pointer
+ *@name: name
+ *@age: age
+ *@owner: owner
+ *Return: pointer
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newDog;
-	char *cpname = NULL, *cpowner = NULL;
-	int i = 0, j = 0;
+	dog_t *puppy;
 
-	for (; *(name + i) != '\0'; i++)
-	{};
-	for (; *(owner + j) != '\0'; j++)
-	{};
+puppy = malloc(sizeof(dog_t));
 
-	cpname = malloc(sizeof(char) * (i + 1));
-	if (!cpname)
-	{
-		free(cpname);
-		return (NULL);
-	}
-	for (i = 0; *(name + i) != '\0'; i++)
-		*(cpname + i) = *(name + i);
-	*(cpname + i) = '\0';
-	cpowner = malloc(sizeof(char) * (j + 1));
-	if (!cpowner)
-	{
-		free(cpname);
-		free(cpowner);
-		return (NULL);
-	}
-	for (j = 0; *(owner + j) != '\0'; j++)
-		*(cpowner + j) = *(owner + j);
-	*(cpowner + j) = '\0';
-	newDog = malloc(sizeof(struct dog));
-	if (!newDog)
-	{
-		free(cpname);
-		free(cpowner);
-		free(newDog);
-		return (NULL);
-	}
-	newDog->name = cpname;
-	newDog->age = age;
-	newDog->owner = cpowner;
-	return (newDog);
+if (!puppy)
+{
+	free(puppy);
+	return (NULL);
+}
+puppy->name = _strdup(name);
+if (puppy->name == NULL) 
+{
+free(puppy);
+return (NULL);
+}
+puppy->age = age;
+puppy->owner = _strdup(owner);
+if (puppy->owner == NULL)
+{
+free(puppy->name);
+free(puppy);
+return (NULL);
+}
+return (puppy);
 }
