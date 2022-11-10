438995ac096a3e2360b153d89b4889db24e58294
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-10 15:15:05 -0500

    task 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 /**
  *free_dog - a function that frees dogs
- *@dog_t: struct dog
  *@d: definition of struct
- *
  */
 
 void free_dog(dog_t *d)
 {
+if (!d)
+{return;}
 free(d->name);
 free(d->owner);
 free(d);
