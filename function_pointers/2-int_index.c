3996e3044a0b6194d737773dfd8114141c2d336e
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-11 10:24:23 -0500

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -5,6 +5,7 @@
  *@array: array of int
  *@size: size of array
  *@cmp: function for comparing values
+ *Return - index or -1 if not match
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -12,15 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 
 	if (array == NULL || cmp == NULL || size <= 0)
-		return(-1);
+		return (-1);
 
 	while (i < size)
 	{
 		if ((cmp)(array[i]))
 		{
-			return(i);
+			return (i);
 		}
 		i++;
 	}
-	return(-1);
+	return (-1);
 }
