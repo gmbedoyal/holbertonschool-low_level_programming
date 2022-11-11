3eac8d7b0cbb64c8f4c621244e89400164f51ec6
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-11 12:14:01 -0500

    task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,8 +15,8 @@ unsigned int i;
 va_start(pa, n);
 
 for (i = 0 ; i < n; i++)
-{	
-if (separator == NULL || i == n - 1 )
+{
+if (separator == NULL || i == n - 1)
 printf("%d", va_arg(pa, const unsigned int));
 else
 printf("%d%s", va_arg(pa, const unsigned int), separator);
