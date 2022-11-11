1cec10d6d0f2ccd714826ae12e9cd25e0578b89d
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-10 21:31:42 -0500

    task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ *array_iterator
+ *
+ *
+ *
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		(action)(array[i]);
+		i++;
+	}
+
+}
