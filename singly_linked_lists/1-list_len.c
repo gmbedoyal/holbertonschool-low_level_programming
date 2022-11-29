f901edc6332fc7abb435987a22241d4a87be38e3
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-29 12:05:02 -0500

    task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *list_len - a function that returns the number of elements in a linked list_t list.
+ *@h: pointer to the head of list
+ *Return: number of nodes in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+	
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+return (i);
+}
