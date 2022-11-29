2198b3ae1b6d4e952b87c017932af1c981bce638
gmbedoyal <gmbedoyal@hotmail.com>
2022-11-29 11:52:31 -0500

    task 0

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,24 +2,24 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_c(va_list imprimir)
+void print_c(va_list c)
 {
-printf("%c", (char)va_arg(imprimir, int));
+printf("%c", va_arg(c, int));
 }
 
-void print_i(va_list imprimir)
+void print_i(va_list i)
 {
-printf("%d", va_arg(imprimir, int));
+printf("%d", va_arg(i, int));
 }
 
-void print_f(va_list imprimir)
+void print_f(va_list f)
 {
-printf("%f", (float)va_arg(imprimir, double));
+printf("%f", va_arg(f, double));
 }
 
-void print_s(va_list imprimir)
+void print_s(va_list s)
 {
-char *str = va_arg(imprimir, char *);
+char *str = va_arg(s, char *);
 	if (str)
 	{
 		printf("%s", str);
@@ -36,6 +36,8 @@ printf("(nil)");
 void print_all(const char * const format, ...)
 {
 
+int i, j;
+
 typedef struct estructura 
 {
 	char *ch;
@@ -49,27 +51,28 @@ estruc array[] = {
 		{"s", print_s}
 };
 
-int i = 0;
-int j = 0;
 
 va_list imprimir;
 va_start(imprimir, format);
 
 /*if (!format)
 	return NULL;*/
+i = 0;
 
 while (format[i] && format)
 {	
-	while (j < 4)
+j = 0;
+	while (array[j].ch != NULL)
 	{
-	if (format[i] == array[j].ch)
+	if (*(array[j].ch) == format[i])
 		{
-		array[i].f(imprimir);
+			array[j].f(imprimir);
+			break;
 		}
 		j++;
-		}
+	}
 	i++;
 }
-printf("\n");
 va_end(imprimir);
+printf("\n");
 }
