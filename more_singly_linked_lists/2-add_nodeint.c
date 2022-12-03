afacce232728a0621721713ca908472a42097d0f
Miguel Bedoya <gmbl07@hotmail.com>
2022-12-02 21:04:33 -0500

    task 2

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: pointer to pointer to head
- * n: data
+ * @n: data
  * Return: the address of the new element, or NULL if it failed
  */
 
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	
 	if (new_node == NULL)
 		return (NULL);
-	
+		
 	new_node->n = n;
 	new_node->next = *head;
 
