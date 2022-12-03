76ae673a35a573dc64e2c20cec9a057b3ad0ce4d
Miguel Bedoya <gmbl07@hotmail.com>
2022-12-03 13:23:01 -0500

    task 6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *pop_listint - deletes the head of a linked list
+ *@head: pointer to pointer of the head of list
+ *Return: the head node’s data (n).
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *check_node;
+
+if (head == NULL)
+	return (0);
+
+check_node = (*head); /*no entiendo pq los paréntesis*/
+(*head) = (*head)->next;
+free(check_node);
+
+return((*head)->n);
+}
