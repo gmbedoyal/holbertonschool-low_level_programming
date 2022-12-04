f2a809b720fc4bf6f85d19838d4e7d1658c52a33
Miguel Bedoya <gmbl07@hotmail.com>
2022-12-04 10:31:23 -0500

    task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -6,7 +6,7 @@
  * @head: pointer to the pointer to head of list
  * @idx: index where the new node should be added. Starts at 0
  * @n: data
- * Returns: the address of the new node, or NULL if it failed
+ * Return: the address of the new node, or NULL if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -18,22 +18,22 @@ if (head == NULL)
 	return (NULL);
 
 new = (listint_t *) malloc(sizeof(listint_t));
-if (new== NULL)
+if (new == NULL)
 	return (NULL);
 new->n = n;
 
 if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
+{
+	new->next = *head;
+	*head = new;
+	return (new);
+}
 
 temp = (*head);
 while (i < idx)
 	{
 	if (temp->next != NULL)
-		{temp = temp->next;}
+		temp = temp->next;
 	else
 		return (NULL);
 	i++;
@@ -41,6 +41,6 @@ while (i < idx)
 
 new->next = temp->next;
 temp->next = new;
-	
-return(new);
+
+return (new);
 }
