e51b11067f30f0f89298811c9a1fb0a3169f9504
Miguel Bedoya <gmbl07@hotmail.com>
2022-12-06 15:44:24 -0500

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	int abrir, escr, text_lenght;
 
-	if (text_content = NULL)
+	if (text_content == NULL)
 		return (1);
 
 	if (filename == NULL)
