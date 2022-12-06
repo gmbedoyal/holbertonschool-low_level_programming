f08bb809c084987b939d54870fe4189da3c10f59
Miguel Bedoya <gmbl07@hotmail.com>
2022-12-06 15:39:41 -0500

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,24 +16,24 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	abrir = open (filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	abrir = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (abrir == -1)
-		return(-1);
+		return (-1);
 
 	text_lenght = strlen(text_content);
 
 	if (text_lenght == 0)
-		return(1);
+		return (1);
 
 	escr = write(abrir, text_content, text_lenght);
 	if (escr == -1)
-		{
-			close (abrir);
-			return (-1);
-		}
+	{
+		close (abrir);
+		return (-1);
+	}
 
-	close (abrir);
+	close(abrir);
 
-return(1);
+return (1);
 }
